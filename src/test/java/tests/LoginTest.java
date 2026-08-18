ffc46431b9466c482acb43344317bfffecfdaf92
Kishore Gopi <gopikishore5577@gmail.com>
2026-08-18 16:55:35 +0530

    firefox browser added

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -3,6 +3,8 @@ package tests;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -27,7 +29,17 @@ public class LoginTest {
 
             driver = new ChromeDriver(options);
 
+        } else if (browser.equalsIgnoreCase("firefox")) {
+
+            WebDriverManager.firefoxdriver().setup();
+
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("-headless");
+
+            driver = new FirefoxDriver(options);
+
         } else {
+
             throw new IllegalArgumentException(
                     "Unsupported browser: " + browser);
         }
